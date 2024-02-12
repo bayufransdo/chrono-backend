ab6ca50154e4e31c66c40a86fec35236fd2c6853
Bayu Maulana <bayufransdo@gmail.com>
2024-02-12 11:01:55 +0700

    Refactor | change schema, add basic routing

--- prisma/migrations/20240212032557_/migration.sql
+++ prisma/migrations/20240212032557_/migration.sql
@@ -6,10 +6,10 @@ CREATE TYPE "Priority" AS ENUM ('none', 'important');
 
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "id" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -19,13 +19,12 @@ CREATE TABLE "users" (
 -- CreateTable
 CREATE TABLE "activities" (
     "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
     "description" TEXT NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
-    "time" time NOT NULL,
     "repetition" "Repetition" NOT NULL,
     "priority" "Priority" NOT NULL,
-    "user_id" TEXT NOT NULL,
+    "user_id" VARCHAR(255) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -35,10 +34,10 @@ CREATE TABLE "activities" (
 -- CreateTable
 CREATE TABLE "history_activities" (
     "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
     "description" TEXT NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
-    "user_id" TEXT NOT NULL,
+    "user_id" VARCHAR(255) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import express, { Request, Response } from 'express';
+const prisma = new PrismaClient();
+const app = express();
+
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const password = await bcrypt.hash('babayo', 10);
+    const user = await prisma.user.create({
+      data: {
+        name: 'Bayu Maulana',
+        email: 'bayumaulana@example.com',
+        password: 'bayumaulana',
+      },
+    });
+    res.status(404).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
